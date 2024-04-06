255b6221fd7bda144d26189518e4cdaf6edca2cd
Brandon Yee <6111102+Firebird1029@users.noreply.github.com>
2024-04-06 15:05:53 -0400

    update auth redirects & homepage buttons

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,16 @@
 import NextLink from "next/link";
 import { Box, Button, Container, Heading, Link } from "@chakra-ui/react";
+import createClient from "@/utils/supabase/server";
 
-export default function Home() {
+export default async function Home() {
+  const supabase = createClient();
+
+  // Ensure user is logged in
+  const {
+    data: { user },
+    error,
+  } = await supabase.auth.getUser();
+
   return (
     <Container>
       <Heading mt={4} mb={8}>
@@ -9,32 +18,37 @@ export default function Home() {
       </Heading>
 
       {/* TODO move these links to navbar etc */}
-      <Box mb={16}>
-        <Link as={NextLink} href="/login">
-          <Button colorScheme="blue">Login</Button>
-        </Link>
-        <br />
-        <br />
-        <Link as={NextLink} href="/register">
-          <Button colorScheme="green">Register</Button>
-        </Link>
-      </Box>
 
-      <Box>
-        <Link as={NextLink} href="/account">
-          <Button>Profile</Button>
-        </Link>
-        <br />
-        <br />
-        <Link as={NextLink} href="/create">
-          <Button>Create Game</Button>
-        </Link>
-        <br />
-        <br />
-        <form action="/auth/signout" method="post">
-          <Button type="submit">Sign out</Button>
-        </form>
-      </Box>
+      {user && (
+        <Box mb={16}>
+          <Link as={NextLink} href="/account">
+            <Button>Profile</Button>
+          </Link>
+          <br />
+          <br />
+          <Link as={NextLink} href="/create">
+            <Button colorScheme="blue">Create Game</Button>
+          </Link>
+          <br />
+          <br />
+          <form action="/auth/signout" method="post">
+            <Button type="submit">Sign out</Button>
+          </form>
+        </Box>
+      )}
+
+      {!user && (
+        <Box mb={16}>
+          <Link as={NextLink} href="/login">
+            <Button colorScheme="blue">Login</Button>
+          </Link>
+          <br />
+          <br />
+          <Link as={NextLink} href="/register">
+            <Button colorScheme="green">Register</Button>
+          </Link>
+        </Box>
+      )}
     </Container>
   );
 }
